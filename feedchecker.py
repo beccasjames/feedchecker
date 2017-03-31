0817e5a0cdb71bfcf6382f4a862a71010182f8ef
Becca James <james.becca@me.com>
2017-03-31 16:38:06 -0400

    Use sys.argv for RSS_URL

--- feedchecker.py
+++ feedchecker.py
@@ -1,29 +1,35 @@
+import sys
 import feedparser
 
-RSS_URL = "[ADD RSS FEED URL HERE]"
-feed = feedparser.parse(RSS_URL)
-feed_length = len(feed.entries)
-feed_statement = "Feed length: "
-feed_length_statement =  feed_statement + str(feed_length)
+for RSS_URL in sys.argv[1:]:
 
-if feed.bozo == 0:
-    print("Awesomesauce. This is a well-formed feed!\n")
-else:
-    print("Ruh roh. Potential trouble ahead.\n")
+    feed = feedparser.parse(RSS_URL)
+    feed_length = len(feed.entries)
+    feed_statement = "Feed length: "
+    feed_length_statement =  feed_statement + str(feed_length)
 
-print("Feed title: " + feed.feed.title)
-print("Feed link: " + feed.feed.link)
-print("Feed description: " + feed.feed.description)
-print("Feed published date: " + feed.feed.published)
-print(feed_length_statement)
-print("\n")
+    if feed.bozo == 0:
+        print("Awesomesauce. This is a well-formed feed!\n")
+    else:
+        print("Ruh roh. Potential trouble ahead.\n")
 
-for entry in feed.entries:
-    print(feed.entries[0].title)
-else:
-    print("Hoorah! You've reached the end of the entry titles.\n")
+    print("Feed title: " + feed.feed.title)
+    print("Feed link: " + feed.feed.link)
+    print("Feed description: " + feed.feed.description)
+    print("Feed published date: " + feed.feed.published)
+    print(feed_length_statement)
+    print("\n")
 
-for entry in feed.entries:
-    print(feed.entries[0].description)
-else:
-    print("The end is here. This is the last entry description.\n")
+    i = 0
+    for entry in feed.entries:
+        print(i, feed.entries[i].title)
+        i += 1
+    else:
+        print("Hoorah! You've reached the end of the entry titles.\n")
+
+    i = 0
+    for entry in feed.entries:
+        print(i, feed.entries[i].description)
+        i += 1
+    else:
+        print("The end is here. This is the last entry description.\n")
